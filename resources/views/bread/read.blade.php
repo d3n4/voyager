a8291ed689ee8f49885608fd53f08dde6c7659e0
Mechislav Lipskiy <id110099122@gmail.com>
2018-09-03 12:12:13 +0300

    fixed page_slug view exception

--- resources/views/bread/read.blade.php
+++ resources/views/bread/read.blade.php
@@ -72,7 +72,7 @@
                                 @if(property_exists($rowDetails, 'relationship'))
 
                                     @foreach(json_decode($dataTypeContent->{$row->field}) as $item)
-                                        @if($item->{$row->field . '_page_slug'})
+                                        @if(isset($item->{$row->field . '_page_slug'}))
                                             <a href="{{ $item->{$row->field . '_page_slug'} }}">{{ $item->{$row->field}  }}</a>@if(!$loop->last), @endif
                                         @else
                                             {{ $item->{$row->field}  }}
